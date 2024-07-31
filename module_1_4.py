65991da079886099208d99e0316e76cd259594ba
BezborodovVV37 <blood371993@gmail.com>
2024-07-31 15:34:50 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+my_string = input('Введите Имя:'  )
+print("Здраствуйте,очень приятно познакомиться", my_string.upper().lower())
+print("Здраствуйте,очень приятно познакомиться".replace(' ','') )
+print (my_string [0])
+print (my_string [-1])
+
